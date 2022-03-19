f92ca4479e008e698be94f9f84386d0b3a837234
marc mckernan <marc.mckernan@gmail.com>
2022-03-19 18:28:14 -0500

    initial commit

--- app/webapp.py
+++ app/webapp.py
@@ -1,13 +1,27 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+import platform
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(request: Request):
+    results = {
+        'request': request,
+        'app_version': '0.0.1',
+        'python_version': platform.python_version()
+    }
+
+    return templates.TemplateResponse('home.html', results, 200)
 
 
 @app.get("/items/{item_id}")
